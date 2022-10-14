41395f7cb49b76012cf8e9e6cd2556d2e4cf5f85
mdforhadulislam <mdforhadul44@gmail.com>
2022-10-14 16:58:57 +0600

    :add all

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
-import { getRequestSend } from "../api/RequestSendMethod.js";
 import Footer from "../components/Footer/Footer.jsx";
 import Navbar from "../components/Nav/Navbar.jsx";
 import Spinner from "../components/util/Spinner.jsx";
@@ -9,29 +8,12 @@ import localStorageToGetTokenUser from "../hooks/getLocalStore.js";
 
 function App({ children }) {
   const [storeToken, setStoreToken] = useState(localStorageToGetTokenUser());
-  const [board, setBoard] = useState([
-    {
-      name: "forhad",
-      color: "hello",
-      user: [],
-      _id: "6347c49987a9c94af753e7a2",
-      createdAt: "2022-10-13T07:56:10.056Z",
-      updatedAt: "2022-10-13T07:56:10.056Z",
-      __v: 0,
-    },
-  ]);
   const dispatch = useDispatch();
   const spinner = useSelector((state) => state.auth.spring);
 
   useEffect(() => {
     dispatch({ type: "ADD_TOKEN", payload: { token: storeToken } });
-
-      getRequestSend("https://trello-apps.herokuapp.com/api/v1/boards/", {
-        authorization: storeToken.token,
-      }).then((response) => setBoard(response.data));
-    
-    dispatch({ type: "ALL_BOARD_FETCH", payload: board });
-  }, [storeToken,board]);
+  }, [storeToken]);
 
   return (
     <div className="w-full h-auto">
