9e891b274ee699f87a3bf38f4290ee5ca745d5fd
mdforhadulislam <mdforhadul44@gmail.com>
2023-09-04 10:37:21 +0600

    url update

--- Api/Urls.js
+++ Api/Urls.js
@@ -1,7 +1,7 @@
-export const rootURL = 'http://localhost:5000/';
-export const API_HEALTH_URL = 'http://localhost:5000/health';
+export const rootURL = 'https://ts-3xqu.onrender.com/';
+export const API_HEALTH_URL = 'https://ts-3xqu.onrender.com/health';
 
-export const API_V_1 = 'http://localhost:5000/api/v1/';
+export const API_V_1 = 'https://ts-3xqu.onrender.com/api/v1/';
 
 export const LOGIN_URL = `${API_V_1}auth/login/`;
 export const LOGOUT_URL = `${API_V_1}auth/logout/`;
@@ -16,7 +16,5 @@ export const BOARD_PUT_URL = (id) => `${API_V_1}boards/${id}`;
 
 export const LIST_URL = (id) => `${API_V_1}lists/?bd=${id}`;
 export const S_LIST_URL = (id) => `${API_V_1}lists/${id}`;
-
-
 
 export const Task_URL = (id) => `${API_V_1}tasks/?lt=${id}`;
